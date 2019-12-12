b60f203324460b6e518b675984b836022cb4a988
Marcin Pohl <marcin.pohl@nasa.gov>
2019-12-12 14:45:45 -0500

    script to pull all the info useful for this

---
+++ git_ASCS.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://developer.nasa.gov/ASCS/red_hat_enterprise_linux_7
+git clone https://developer.nasa.gov/ASCS/red_hat_enterprise_linux_8
+git clone https://developer.nasa.gov/ASCS/red_hat_enterprise_linux_6
+git clone https://developer.nasa.gov/ASCS/openscap/
+git clone https://developer.nasa.gov/ASCS/openssh
+git clone https://developer.nasa.gov/ASCS/organization-defined_values
+git clone https://developer.nasa.gov/ASCS/ssg_tailored
+git clone https://developer.nasa.gov/ASCS/bigfix
+git clone https://developer.nasa.gov/ASCS/anyconnect
+git clone https://developer.nasa.gov/ASCS/opensc
+git clone https://developer.nasa.gov/ASCS/chrome
+git clone https://developer.nasa.gov/ASCS/firefox
+git clone https://developer.nasa.gov/ASCS/bios
+git clone https://developer.nasa.gov/ASCS/scap-security-guide
+git clone https://developer.nasa.gov/ASCS/ansible_baselines
